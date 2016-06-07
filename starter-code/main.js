cf96189f774f176bab344f739ce92f04096e0b17
austinhudson <AustinHudson@Austins-Air.home>
2016-06-07 18:20:26 -0400

    added files from Units 1-5and created a main.js file to represent cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,5 @@
-console.log("JS file is connected to HTML! Woo!")
+//variables to represent the cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
