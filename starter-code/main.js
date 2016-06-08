3acba19a449f38b6a0ee2ba58583141f1c46d6fc
austinhudson <AustinHudson@Austins-Air.home>
2016-06-07 20:48:20 -0400

    added some logic to the game

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,28 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+if(cardOne === cardTwo){
+	alert("You found a match!"); 
+}
+
+if else (cardOne !== cardThree){
+	alert("Sorry, try again.");
+}
+
+if else (cardOne !== cardFour){
+	alert("Sorry, try again.");
+}
+
+if else (cardTwo !== cardThree){
+	alert("Sorry, try again.");
+}
+
+if else(cardTwo != cardFour){
+	alert("Sorry, try again.");
+}
+
+if else(cardThree === cardFour){
+	alert("You found a match");
+}
