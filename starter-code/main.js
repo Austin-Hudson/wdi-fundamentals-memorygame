02c32c968abf0433a1af8cfead342c18734cf2ac
austinhudson <AustinHudson@Austins-Air.home>
2016-06-09 13:34:25 -0400

    added a js function to create the board

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,12 +4,37 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+//card length
+var cardLength = 4; 
 
+
+//function to create board
+var createBoard = function() {
+
+	//get the board 
+	var gameBoard = document.getElementById("game-board");
+
+	//create div elements representing the number of cards
+	for(var i=0; i<cardLength; i++)
+	{
+		var divItem = document.createElement('div');
+		divItem.className = 'card';
+		gameBoard.appendChild(divItem);
+
+	}
+
+
+}
+
+//call the create board
+createBoard(); 
+
+/*
 if(cardOne === cardTwo){
 	alert("You found a match!"); 
 }
 
-/*
+
 if else (cardOne !== cardThree){
 	alert("Sorry, try again.");
 }
